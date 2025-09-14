00de4099f44ca92c56b6de305fe22c13fe5d0474
dickxumamba <xuruoyu27@gmail.com>
2025-09-14 21:01:06 +0800

    Thinking about the first function still

--- lab6.cpp
+++ lab6.cpp
@@ -2,19 +2,26 @@
 #include <iomanip>
 using namespace std;
 
+const int SIZE = 5; // the size of array is 5
+
 void enterArrayData(double* arr, int n);
 void outputArrayData(double* arr, int n);
 double sumArray(double* arr, int n);
 
 int main() {
     
-    const int SIZE = 5; // the size of array is 5
+    //
     
     return 0;
 }
 
 void enterArrayData(double* arr, int n){
-    //
+    cout << "Data entry for the array: "<<endl;
+
+    for (int i = 0; i < SIZE; i++){
+        cin 
+    }
+
 }
 
 void outputArrayData(double* arr, int n){
