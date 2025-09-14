c626763e0b255b8e3f090f7ceeb54874b2989fe0
dickxumamba <xuruoyu27@gmail.com>
2025-09-14 20:32:09 +0800

    Start the lab6 now, first time structure commit

---
+++ lab6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+using namespace std;
+
+void enterArrayData(double* arr);
+void outputArrayData(double* arr);
+
+
+int main() {
+    
+    //
+    
+    return 0;
+}
