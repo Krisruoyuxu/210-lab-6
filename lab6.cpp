0cc350b94b945f055d8ea08c7c6ec9e56b22247c
dickxumamba <xuruoyu27@gmail.com>
2025-09-14 20:49:05 +0800

    still thinking....so only few change made

--- lab6.cpp
+++ lab6.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <ctime>
+#include <iomanip>
 using namespace std;
 
-void enterArrayData(double* arr);
-void outputArrayData(double* arr);
-double sumArray(double* arr);
+void enterArrayData(double* arr, int n);
+void outputArrayData(double* arr, int n);
+double sumArray(double* arr, int n);
 
 int main() {
     
-    //
+    const int SIZE = 5; // the size of array is 5
     
     return 0;
+}
+
+void enterArrayData(double* arr, int n){
+    //
+}
+
+void outputArrayData(double* arr, int n){
+    //
+}
+
+double sumArray(double* arr, int n){
+    //
 }
